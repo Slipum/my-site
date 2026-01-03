d6d28a4b5a8bb1ea7f5ea0018d4e1184825d9577
Slipum <sabirovdanil7@gmail.com>
2026-01-03 16:30:51 +0300

    feat: add more svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css';
+import logoSlp from './assets/logo-slipum.svg';
 
 function App() {
 	return (
 		<>
-			<h1>Hey there! I'm Slipum</h1>
-			<h2>
-				I am <p>FullStack</p> Developer
-			</h2>
+			<div className="main-info">
+				<img
+					src={logoSlp}
+					alt="Slipum"
+					style={{ width: '250px', height: '250px', borderRadius: '48px' }}
+				/>
+				<h1>Hey there! I'm Slipum</h1>
+				<h2>
+					I am <p>FullStack</p> Developer
+				</h2>
+			</div>
 		</>
 	);
 }
